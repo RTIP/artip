26fce12be29d233347e3f4723a788303ec419529
Dollyg <dollyg@thoughtworks.com>
2016-10-13 17:27:54 +0530

    Prashant/Dolly/Shripad : Added logic for linear shifting of angles to avoid bimodal distribution

--- src/main/python/start.py
+++ src/main/python/start.py
@@ -23,27 +23,34 @@ def plot_percentiles(data, antenna):
 
 def toDegree(angleInRadians):
     angleInDegree = int(angleInRadians * (180 / 3.14))
-    if angleInDegree<0 :
-        return 360-abs(angleInDegree)
-    else:
-        return angleInDegree
+    return  angleInDegree
+    # if angleInDegree<0 :
+    #     return 360-abs(angleInDegree)
+    # else:
+    #     return angleInDegree
+
+def shiftToZero(data):
+    degreeOfRotation = min(data)
+    return map(lambda pd: pd+abs(degreeOfRotation), data)
 
 
 def main(args=None):
     ms = casac.casac.ms ()
     ms.open('/Users/dollyg/Projects/IUCAA/output/may14.ms')
     ms.selectinit(reset=True)
-    antennas = filter(lambda antenna: antenna!=2, range(3, 29, 1))
-    print antennas
-    for antenna1 in antennas:
+    antennas = filter(lambda antenna: antenna!=2, range(1, 29, 1))
+
+    for antenna in antennas:
         ms.selectinit(reset=True)
         ms.selectpolarization('RR')
         ms.selectchannel(start=100)
-        ms.select({'scan_number': 1, 'antenna1': 2, 'antenna2': antenna1})
+        ms.select({'scan_number': 1, 'antenna1': 0, 'antenna2': antenna})
         phase_data = ms.getdata(['phase'])['phase'][0][0]
         phase_data_in_degrees = map(lambda pd: toDegree(pd), phase_data)
-        plot_pdf(phase_data_in_degrees, antenna1)
-        print antenna1, stats.variation(phase_data_in_degrees)
+
+        new_phase = shiftToZero(phase_data_in_degrees)
+        plot_pdf(new_phase, antenna)
+        print antenna, stats.variation(new_phase)
         print phase_data_in_degrees
 
 
