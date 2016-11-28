1a7fa597d1fcb30967d96f34ae0c5529b340bd54
Ravi Sharma <ravis4376@gmail.com>
2016-11-28 13:30:43 +0530

    [#14] | Dolly/Ravi | Fixed threshold assertion in config tests

--- src/unittest/python/config_tests.py
+++ src/unittest/python/config_tests.py
@@ -13,7 +13,7 @@ class ConfigTest(unittest.TestCase):
         self.assertEqual(self.config.global_configs(), expected)
 
     def test_should_return_configs_for_flux_calibration(self):
-        expected = {'field': 0, 'channel': 100, 'r_threshold': 0.5}
+        expected = {'field': 0, 'channel': 100, 'r_threshold': 0.3}
         self.assertEqual(self.config.get('flux_calibration'), expected)
 
     def test_should_return_error_when_invalid_config_name_is_passed(self):
