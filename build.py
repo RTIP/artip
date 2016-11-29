f25331ff1fd81edd3b225eb95b36859c9bbd4496
Ravi Sharma <ravis4376@gmail.com>
2016-11-29 22:29:15 +0530

    [#Tech] Ravi - added unit test coverage tool, the threshold warnining is currently set to 60%. It also prints 'Coverage File Path' in pyb log.

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
-from pybuilder.core import use_plugin, init
+import os
+from pybuilder.core import use_plugin, init, task
 from sys import path
 path.append("src/main/python")
 
@@ -6,11 +7,21 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
+use_plugin("python.coverage")
 
 name = "artip"
 default_task = "publish"
 
 @init
+def set_dependencies(project):
+    # Build dependencies
+    project.build_depends_on('mock')
+
+@init
 def set_properties(project):
-    print("Init task")
-    pass
+  project.set_property('coverage_threshold_warn', 60)
+
+
+@task
+def run_unit_tests(logger):
+   logger.info("Coverage File Path : %s/target/reports/coverage_html/index.html",os.getcwd())
