d180a5a9a0cb24eddea77bcea759d457e78326a0
Ravi Sharma <ravis4376@gmail.com>
2016-12-07 17:47:09 +0530

    [#33] formatted report output.

--- src/main/python/report.py
+++ src/main/python/report.py
@@ -6,8 +6,10 @@ class Report:
         self.__antennas = antennas
 
     def generate_report(self, scan_ids):
-        print "AntennaId,Polarisation,ScanId,R_Status,CP_Status"
+        print "AntennaId, Polarisation, ScanId, R_Status, CP_Status"
         for ant in self.__antennas:
             for state in ant.get_states():
                 if state.scan_id in scan_ids and (state.get_R_phase_status() == AntennaStatus.BAD or state.get_closure_phase_status() == AntennaStatus.BAD):
-                    print ant.id, state.polarization, state.scan_id, state.get_R_phase_status(), state.get_closure_phase_status()
+                    print "   {0}\t   \t{1}\t   {2}\t   {3}\t     {4}".format(
+                        ant.id, state.polarization, state.scan_id, state.get_R_phase_status(), state.get_closure_phase_status())
+                    # print ant.id, state.polarization, state.scan_id, state.get_R_phase_status(), state.get_closure_phase_status()
