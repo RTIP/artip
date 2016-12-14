0e89eef107e3bc6983ce078fe87ec42a5124de44
Dollyg <dollyg@thoughtworks.com>
2016-12-14 11:14:09 +0530

    [#Misc] | Modified the import csv script to work for all combinations of antenna pairs.

--- resources/import_data_into_csv.py
+++ resources/import_data_into_csv.py
@@ -5,11 +5,12 @@ import itertools
 data = {
     'input_location': '',
     'csv_location': '',
-    'channel': 60,
+    'channel': 100,
     'polarization': 'RR',
-    'antenna1': 2,
+    'antenna1': range(0,29,1),
     'antenna2': range(0,29,1),
-    'scan_number': 1
+    'scan_number': 1,
+    'selection': 'corrected_amplitude'
 }
 
 
@@ -20,27 +21,26 @@ def main(args=None):
         writer = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-        antenna1 = data['antenna1']
-        for antenna2 in data['antenna2']:
-            ms.selectinit(reset=True)
-            ms.selectpolarization(data['polarization'])
-            ms.selectchannel(start=data['channel'])
+        for antenna1 in data['antenna1']:
+            for antenna2 in data['antenna2']:
+                ms.selectinit(reset=True)
+                ms.selectpolarization(data['polarization'])
+                ms.selectchannel(start=data['channel'])
 
-            if antenna1 == antenna2:
-                continue
-            elif antenna1 < antenna2:
-                primary_antenna = antenna1
-                secondary_antenna = antenna2
-            else:
-                primary_antenna = antenna2
-                secondary_antenna = antenna1
+                if antenna1 == antenna2:
+                    continue
+                elif antenna1 < antenna2:
+                    primary_antenna = antenna1
+                    secondary_antenna = antenna2
+                else:
+                    primary_antenna = antenna2
+                    secondary_antenna = antenna1
 
-            ms.select({'scan_number': data['scan_number'], 'antenna1': primary_antenna, 'antenna2': secondary_antenna})
-            all_data = ms.getdata(['amplitude'])
-            amp_data = all_data['amplitude'][0][0]
-            flattened_data = list(itertools.chain(*[[primary_antenna, secondary_antenna], amp_data]))
-
-            writer.writerow(flattened_data)
+                ms.select({'scan_number': data['scan_number'], 'antenna1': primary_antenna, 'antenna2': secondary_antenna})
+                all_data = ms.getdata([data['selection']])
+                amp_data = all_data[data['selection']][0][0]
+                flattened_data = list(itertools.chain(*[[primary_antenna, secondary_antenna], amp_data]))
+                writer.writerow(flattened_data)
 
 
 main()
