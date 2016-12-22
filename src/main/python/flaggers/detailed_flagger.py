403e5c23c2f065e8ec255d5ab35991e7aaa94a47
Ravi Sharma <ravis4376@gmail.com>
2016-12-22 18:50:22 +0530

    [#Refactoring] - extracted out common code.

--- src/main/python/flaggers/detailed_flagger.py
+++ src/main/python/flaggers/detailed_flagger.py
@@ -12,7 +12,7 @@ class DetailedFlagger:
         self.measurement_set = measurement_set
 
     def get_bad_antennas(self, source):
-        antenna_deviation = time_deviation = baseline_deviation = {}
+        dataset_deviations = {}
         polarizations = GLOBAL_CONFIG['polarizations']
         source_config = ALL_CONFIGS[source]
         scan_ids = self.measurement_set.scan_ids_for(source_config['field'])
@@ -30,46 +30,32 @@ class DetailedFlagger:
             print '---------------------------'
 
             unflagged_antennaids = self.measurement_set.unflagged_antennaids(polarization, scan_id)
-            for antenna_id in unflagged_antennaids:
-                antenna_matrix = amp_matrix.filter_by_antenna(antenna_id)
-                if not antenna_matrix.is_empty():
-                    antenna_median = antenna_matrix.median()
-                    antenna_mad = antenna_matrix.mad()
-                    antenna_deviation[antenna_id] = abs(ideal_median - antenna_median)
-            antenna_median_deviation = calculate_median(antenna_deviation.values())
-            for antenna_id in antenna_deviation:
-                deviation_ratio = (antenna_deviation[antenna_id] / antenna_median_deviation)
-                if deviation_ratio > 5:
-                    print "Bad Antenna=", antenna_id
-                elif deviation_ratio > 3:
-                    print "Border line Antenna=", antenna_id
+
+            self._filter_bad_data_for('Antenna', amp_matrix.filter_by_antenna, unflagged_antennaids, dataset_deviations,
+                                      ideal_median)
+            print '---------------------------'
+            self._filter_bad_data_for('Time', amp_matrix.filter_by_time, range(0, amp_matrix.readings_count()),
+                                      dataset_deviations,
+                                      ideal_median)
 
             print '---------------------------'
-            for index in range(0, amp_matrix.readings_count()):
-                time_matrix = amp_matrix.filter_by_time(index)
-                timeline_median = time_matrix.median()
-                timeline_mad = time_matrix.mad()
-                time_deviation[index] = abs(ideal_median - timeline_median)
-            time_median_deviation = calculate_median(time_deviation.values())
-            for time_index in time_deviation:
-                deviation_ratio = (time_deviation[time_index] / time_median_deviation)
-                if deviation_ratio > 5:
-                    print "Bad Time=", time_index
-                elif deviation_ratio > 3:
-                    print "Border line Time=", time_index
-
-            print '---------------------------'
-            for base_line in amp_matrix.amplitude_data_matrix:
-                baseline_matrix = amp_matrix.filter_by_baseline(base_line)
-                baseline_median = baseline_matrix.median()
-                baseline_mad = baseline_matrix.mad()
-                baseline_deviation[base_line] = abs(ideal_median - baseline_median)
-            baseline_median_deviation = calculate_median(baseline_deviation.values())
-
-            for baseline in baseline_deviation:
-                deviation_ratio = (baseline_deviation[baseline] / baseline_median_deviation)
-                if deviation_ratio > 5:
-                    print "Bad Baseline=", baseline
-                elif deviation_ratio > 3:
-                    print "Border line Baseline=", baseline
+            self._filter_bad_data_for('Baseline', amp_matrix.filter_by_baseline, amp_matrix.amplitude_data_matrix,
+                                      dataset_deviations, ideal_median)
             print '****************************'
+
+    def _filter_bad_data_for(self, element_name, filter, on_dataset, dataset_deviations, ideal_median):
+        for element in on_dataset:
+            filtered_matrix = filter(element)
+            median = filtered_matrix.median()
+            mad = filtered_matrix.mad()
+            dataset_deviations[element] = abs(ideal_median - median)
+        dataset_deviations_median = calculate_median(dataset_deviations.values())
+        self._identify_bad_data(element_name, dataset_deviations, dataset_deviations_median)
+
+    def _identify_bad_data(self, element_name, dataset_deviations, dataset_deviations_median):
+        for element in dataset_deviations:
+            deviation_ratio = (dataset_deviations[element] / dataset_deviations_median)
+            if deviation_ratio > 5:
+                print 'Bad ', element_name, '=', element
+            elif deviation_ratio > 3:
+                print 'Border line', element_name, '=', element
