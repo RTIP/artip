0afb9b1a1ddf564bca3c17c966f01215282c11bf
Ravi Sharma <ravis4376@gmail.com>
2016-12-27 13:58:42 +0530

    [#Refactoring] Removed unwanted print statement.

--- src/main/python/report.py
+++ src/main/python/report.py
@@ -12,4 +12,3 @@ class Report:
                 if state.scan_id in scan_ids and (state.get_R_phase_status() == AntennaStatus.BAD or state.get_closure_phase_status() == AntennaStatus.BAD):
                     print "   {0}\t   \t{1}\t   {2}\t   {3}\t     {4}".format(
                         ant.id, state.polarization, state.scan_id, state.get_R_phase_status(), state.get_closure_phase_status())
-                    print ant.id, state.polarization, state.scan_id, state.get_R_phase_status(), state.get_closure_phase_status()
