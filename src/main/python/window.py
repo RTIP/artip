b11fa47005379be62723c4a86970d69724d28238
Ravi Sharma <ravis4376@gmail.com>
2016-12-27 12:25:28 +0530

    [#33] + Dolly | modified Window class to take multiple key data as input.

--- src/main/python/window.py
+++ src/main/python/window.py
@@ -1,24 +1,26 @@
 from amplitude_matrix import AmplitudeMatrix
+import numpy
 
 
 class Window:
     def __init__(self, collection, bucket_size, overlap_count):
-        self.__key = collection.keys()[0]
-        self.__values = collection.values()[0]
+        self.__collection = collection
         self.__bucket_size = bucket_size
         self.__overlap_count = overlap_count
         self.__window_start_index = 0
 
     def slide(self):
-        # print "BaseLine=",self.__key," Window start=", self.__window_start_index, "  Window End=",(self.__window_start_index + self.__bucket_size)
-        window_data = self.__values[self.__window_start_index:self.__window_start_index + self.__bucket_size]
-        if len(window_data) > 0:
+        window_data = dict(
+            (baseline, amp_data[self.__window_start_index:self.__window_start_index + self.__bucket_size]) for
+            baseline, amp_data in self.__collection.iteritems())
+        if len(numpy.array(window_data.values()).flatten()) > 0:
             self.__window_start_index = (self.__window_start_index + self.__bucket_size) - self.__overlap_count
-        return AmplitudeMatrix(None, None, None, None, {self.__key: window_data})
+        return AmplitudeMatrix(None, None, None, None, window_data)
 
     def current_position(self):
+        readings_count = len(self.__collection[self.__collection.keys()[0]])
         start = (self.__window_start_index - self.__bucket_size) + self.__overlap_count
         end = (start + self.__bucket_size) - 1
-        if end > len(self.__values):
-            end = len(self.__values) - 1
+        if end > readings_count:
+            end = readings_count - 1
         return start, end
