11aa925b1036617c189f5b1077a0791cc9a5cd6e
Dollyg <dollyg@thoughtworks.com>
2017-01-04 14:24:04 +0530

    [Misc] | +Ravi | Fixing config test

--- src/unittest/python/configs_tests/config_loader_tests.py
+++ src/unittest/python/configs_tests/config_loader_tests.py
@@ -6,5 +6,5 @@ class ConfigTest(unittest.TestCase):
     def test_should_load_the_given_config_file(self):
         config = ConfigLoader().load('conf/config.yml')
         actual_properties = config.keys()
-        expected_properties = ['global', 'flux_calibration']
+        expected_properties = ['global', 'flux_calibration', 'casapy']
         self.assertItemsEqual(expected_properties, actual_properties)
