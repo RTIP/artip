776d0241e6444f5d889728327426077c401d17dd
Ravi Sharma <ravis4376@gmail.com>
2017-01-19 18:50:01 +0530

    added config for running casapy on different platforms.

--- src/main/python/configs/config.py
+++ src/main/python/configs/config.py
@@ -1,7 +1,8 @@
 from config_loader import ConfigLoader
+import platform
 
 ALL_CONFIGS = ConfigLoader().load('conf/config.yml')
 GLOBAL_CONFIG = ALL_CONFIGS['global']
 DETAIL_FLAG_CONFIG = ALL_CONFIGS['flux_calibration']['detail_flagging']
 DATASET = None
-CASAPY_CONFIG = ALL_CONFIGS['casapy']
+CASAPY_CONFIG = ALL_CONFIGS['casapy'][platform.system()]
