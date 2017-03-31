1f14d741c029a03eaf70991edbbedac9dc2700bd
Dollyg <dollyg@thoughtworks.com>
2017-03-31 15:44:49 +0530

    +Ravi | Added assertions for calibration using ms stats

--- src/integrationtest/python/flagging_tests.py
+++ src/integrationtest/python/flagging_tests.py
@@ -1,24 +1,22 @@
 import unittest
 from configs import config, pipeline_config, logging_config
+from configs.config_loader import ConfigLoader
 import start
 from main import main
 from os import listdir
-from shutil import rmtree
-from test_helper import fits_to_ms
+from test_helper import *
 
 
 class FlaggingTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        pipeline_config.load("src/integrationtest/conf/pipeline_config.yml")
-        logging_config.load()
-
     def __init__(self, dataset_name):
         unittest.TestCase.__init__(self, methodName='test_actual_flags_should_match_expected_flags')
         self.seed_data_path = "src/integrationtest/seed_data/"
         self.dataset_name = dataset_name
 
     def setUp(self):
+        pipeline_config.load("src/integrationtest/conf/pipeline_config.yml")
+        logging_config.load()
+
         config.load(self.seed_data_path + self.dataset_name + "/config.yml")
         self.ms_file = "{0}/{1}/{1}.ms".format(config.OUTPUT_PATH, self.dataset_name)
         fits_file = "{0}/{1}/{1}.fits".format(self.seed_data_path, self.dataset_name)
@@ -27,13 +25,26 @@ class FlaggingTest(unittest.TestCase):
 
     def test_actual_flags_should_match_expected_flags(self):
         self.assert_flagging()
+        self.assert_calibration()
 
     def assert_flagging(self):
+        pipeline_config.STAGES_CONFIG.update({'flux_calibration': True, 'bandpass_calibration': True,
+                                              'phase_calibration': True})
         main(self.ms_file)
+
         actual_flags = open(config.OUTPUT_PATH + '/flags.txt').read()
         expected_flags = open("{0}/{1}/expected_flags.txt".format(self.seed_data_path, self.dataset_name)).read()
+
         self.assertEquals(actual_flags, expected_flags, msg="Dataset= " + self.ms_file)
 
+
+    def assert_calibration(self):
+        actual_stats = get_stats(self.ms_file, config.GLOBAL_CONFIG['flux_cal_fields'])
+        expected_stats_path = "{0}/{1}/expected_stats.yml".format(self.seed_data_path, self.dataset_name)
+        expected_stats = ConfigLoader().load(expected_stats_path)['calibration']
+
+        self.assertTrue(is_subset(actual_stats, expected_stats),
+                        msg="Dataset={0}, actual={1}, expected={2}".format(self.ms_file, actual_stats, expected_stats))
 
 def get_test_data_suite():
     dataset_names = filter(lambda file_name: not file_name.startswith('.'), listdir('src/integrationtest/seed_data/'))
--- src/integrationtest/python/test_helper.py
+++ src/integrationtest/python/test_helper.py
@@ -1,14 +1,24 @@
 from configs import config
-import os
-import subprocess,time
+import casac
+import subprocess, time
+
 
 def fits_to_ms(fits_file, ms_path):
     logfile = config.OUTPUT_PATH + "/casa.log"
     casapy_path = config.CASAPY_CONFIG['path']
     import_task = "importgmrt(fitsfile='{0}',vis='{1}')".format(fits_file, ms_path)
     command = "{0} --nologger --nogui  --logfile {1} -c \"{2}\"".format(casapy_path, logfile, import_task)
-    # os.system(command)
 
     process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
     while process.poll() is None:
         time.sleep(1)
+
+
+def get_stats(ms_path, fields):
+    ms = casac.casac.ms()
+    ms.open(ms_path)
+    return ms.statistics(column="CORRECTED_DATA", complex_value='amp', field=",".join(map(str,fields)))['CORRECTED']
+
+
+def is_subset(superset, subset):
+    return set(subset.items()).issubset(set(superset.items()))
