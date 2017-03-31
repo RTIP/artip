406f385521747a3d5aa9755351440bd9ebbc9585
Dollyg <dollyg@thoughtworks.com>
2017-03-31 11:26:38 +0530

    +Ravi | Made necessary corrections to the flagging test expectations

--- src/integrationtest/python/flagging_tests.py
+++ src/integrationtest/python/flagging_tests.py
@@ -17,32 +17,29 @@ class FlaggingTest(unittest.TestCase):
         unittest.TestCase.__init__(self, methodName='test_actual_flags_should_match_expected_flags')
         self.seed_data_path = "src/integrationtest/seed_data/"
         self.dataset_name = dataset_name
-        config.load(self.seed_data_path + dataset_name + "/config.yml")
 
-        self.ms_file = "{0}/{1}/{1}.ms".format(config.OUTPUT_PATH, dataset_name)
-        fits_file = "{0}/{1}/{1}.fits".format(self.seed_data_path, dataset_name)
+    def setUp(self):
+        config.load(self.seed_data_path + self.dataset_name + "/config.yml")
+        self.ms_file = "{0}/{1}/{1}.ms".format(config.OUTPUT_PATH, self.dataset_name)
+        fits_file = "{0}/{1}/{1}.fits".format(self.seed_data_path, self.dataset_name)
         start.create_output_dir(fits_file)
         fits_to_ms(fits_file, self.ms_file)
 
     def test_actual_flags_should_match_expected_flags(self):
+        self.assert_flagging()
+
+    def assert_flagging(self):
         main(self.ms_file)
         actual_flags = open(config.OUTPUT_PATH + '/flags.txt').read()
         expected_flags = open("{0}/{1}/expected_flags.txt".format(self.seed_data_path, self.dataset_name)).read()
         self.assertEquals(actual_flags, expected_flags, msg="Dataset= " + self.ms_file)
-
-    def shortDescription(self):
-        return 'Testing for... ' + self.dataset_name
-
-    @classmethod
-    def tearDownClass(cls):
-        rmtree(config.OUTPUT_PATH)
 
 
 def get_test_data_suite():
     dataset_names = filter(lambda file_name: not file_name.startswith('.'), listdir('src/integrationtest/seed_data/'))
     return unittest.TestSuite([FlaggingTest(dataset) for dataset in dataset_names])
 
+
 if __name__ == '__main__':
     testRunner = unittest.TextTestRunner()
     testRunner.run(get_test_data_suite())
-
