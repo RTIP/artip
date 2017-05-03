1bf7a9484022818ffa5f74840dc9b6654e6e7608
Sarang Sanjay Kulkarni <ksarang@thoughtworks.com>
2017-05-03 17:04:13 +0530

    [#Multiple_Target] Modifying config.yml for supporting multiple target source.

--- casa_scripts/apply_line_calibration.py
+++ casa_scripts/apply_line_calibration.py
@@ -1,12 +1,13 @@
 import sys
 
+source_id = sys.argv[-5]
 ms_dataset = sys.argv[-4]
 output_path = sys.argv[-3]
 p_loop_count = sys.argv[-2]
 ap_loop_count = sys.argv[-1]
 
-p_table = '{0}/self_caled_p/p_selfcaltable_{1}.gcal'.format(output_path, p_loop_count)
-ap_table = '{0}/self_caled_ap/ap_selfcaltable_{1}.gcal'.format(output_path, ap_loop_count)
+p_table = '{0}/self_caled_p_{1}/p_selfcaltable_{2}.gcal'.format(output_path, source_id, p_loop_count)
+ap_table = '{0}/self_caled_ap_{1}/ap_selfcaltable_{2}.gcal'.format(output_path, source_id,  ap_loop_count)
 
 applycal(vis=ms_dataset, field='0', gaintable=[p_table, ap_table],
          gainfield=['0', '0'], calwt=F)
