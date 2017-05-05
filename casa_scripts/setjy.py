a0d1596fa5a07c44a843a8f84170d87cbd955b4a
Dollyg <dollyg@thoughtworks.com>
2017-05-05 11:56:36 +0530

    +Sarang | Added usescratch for setjy to create new model column everytime to work for datasets with multiple spws

--- casa_scripts/setjy.py
+++ casa_scripts/setjy.py
@@ -4,4 +4,4 @@ ms_dataset = sys.argv[-3]
 spw = sys.argv[-4]
 field = sys.argv[-2]
 model_path = sys.argv[-1]
-setjy(vis=ms_dataset, field=field, spw=spw, modimage=model_path)
+setjy(vis=ms_dataset, field=field, spw=spw, modimage=model_path, usescratch=True)
