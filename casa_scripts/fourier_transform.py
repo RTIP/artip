87c412e3581e648d98e5d600bd1388aaa26c2136
Sarang Sanjay Kulkarni <ksarang@thoughtworks.com>
2017-05-25 13:00:09 +0530

    [+Ravi] Add spw list in ft task instead of spw_range.

--- casa_scripts/fourier_transform.py
+++ casa_scripts/fourier_transform.py
@@ -7,12 +7,7 @@ loop_count = sys.argv[-3]
 ms_dataset = sys.argv[-2]
 field_name = sys.argv[-1]
 
-spw_range = spw_range.split("~")
-
-for spw in range(int(spw_range[0]), int(spw_range[-1]) + 1):  # to be modified
-
+for spw in spw_range.split(","):
     image_path = '{0}/self_cal_image'.format(output_path)
-
     model_name = "{0}_{1}_{2}_spw{3}.model".format(image_path, cal_mode, loop_count, spw)
-
-    ft(vis=ms_dataset, field=field_name, spw=str(spw), model=model_name)
+    ft(vis=ms_dataset, field=field_name, spw=spw, model=model_name)
