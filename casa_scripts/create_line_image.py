5c67f68bc34f41329802ba75a97040350e4900af
Sarang Sanjay Kulkarni <ksarang@thoughtworks.com>
2017-05-29 17:22:28 +0530

    [+Ravi] Now creating single line and continuum images using all spws.

--- casa_scripts/create_line_image.py
+++ casa_scripts/create_line_image.py
@@ -14,12 +14,12 @@ cell = sys.argv[-2]
 niter = int(sys.argv[-1])
 field = '0'
 
-for spw in spw_list.split(","):
-    fitspw = "0:{0}".format(spw, fitspw_channels)
-    image_name = "{0}/line_spectral_image_spw{1}".format(image_path, spw)
-    spw_line_ms = "{0}/line_spw{1}.ms".format(image_path, spw)
-    split(vis=ms_input, outputvis=spw_line_ms, spw=spw, datacolumn='corrected')
-    uvcontsub(vis=spw_line_ms, field=field, spw='0', fitspw=fitspw, solint='int', fitorder=0)
-    clean(vis="{0}.contsub".format(spw_line_ms), imagename=image_name, imagermode='csclean', imsize=imsize, cell=[cell],
-          mode='channel', weighting='briggs', robust=robust, threshold=threshold, interactive=interactive, niter=niter,
-          spw='0')
+fitspw = ",".join(["{0}:{1}".format(s, fitspw_channels) for s in spw_list.split(",")])
+
+image_name = "{0}/line_spectral_image".format(image_path)
+corrected_line_ms = "{0}/corrected_line.ms".format(image_path)
+split(vis=ms_input, outputvis=corrected_line_ms, spw=spw_list, datacolumn='corrected')
+uvcontsub(vis=corrected_line_ms, field=field, spw=spw_list, fitspw=fitspw, solint='int', fitorder=0)
+clean(vis="{0}.contsub".format(corrected_line_ms), imagename=image_name, imagermode='csclean', imsize=imsize,
+      cell=[cell], mode='channel', weighting='briggs', robust=robust, threshold=threshold, interactive=interactive,
+      niter=niter, spw=spw_list)
--- casa_scripts/fourier_transform.py
+++ casa_scripts/fourier_transform.py
@@ -7,7 +7,6 @@ loop_count = sys.argv[-3]
 ms_dataset = sys.argv[-2]
 field_name = sys.argv[-1]
 
-for spw in spw_range.split(","):
-    image_path = '{0}/self_cal_image'.format(output_path)
-    model_name = "{0}_{1}_{2}_spw{3}.model".format(image_path, cal_mode, loop_count, spw)
-    ft(vis=ms_dataset, field=field_name, spw=spw, model=model_name)
+image_path = '{0}/self_cal_image'.format(output_path)
+model_name = "{0}_{1}_{2}.model".format(image_path, cal_mode, loop_count)
+ft(vis=ms_dataset, field=field_name, spw=spw_range, model=model_name)
