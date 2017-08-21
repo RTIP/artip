1ec92b6ed7d4b031b726a0ed13918ae50f75f70a
Arti Anant Pande <artip@INartip.local>
2017-08-21 13:26:52 +0530

    [#Spike-2]Import MS data into a CSV

--- resources/import_data_into_csv.py
+++ resources/import_data_into_csv.py
@@ -1,50 +1,39 @@
 import casac
 import csv
-import itertools
 
-data = {
-    'input_location': '/Users/dollyg/Projects/IUCAA/output/aug7.ms',
-    'csv_location': '/Users/dollyg/Projects/IUCAA/output/aug7.csv',
+inputs = {
+    'input_location': '/Users/artip/Projects/iucaa/artip/data/copies/may14.ms',
+    'csv_location': '/Users/artip/Projects/iucaa/artip/data/copies/may14.csv',
     'channel': 100,
     'polarization': 'RR',
-    'antenna1': range(0,29,1),
-    'antenna2': range(0,29,1),
+    'antenna1': range(0, 29, 1),
+    'antenna2': range(0, 29, 1),
     'scan_number': 1,
-    'selection': 'time'
+    'selection': ['amplitude', 'time', 'antenna1', 'antenna2']
 }
 
 
+def get_data():
+    ms = casac.casac.ms()
+    ms.open(inputs['input_location'])
+    ms.selectinit(reset=True)
+    ms.selectpolarization(inputs['polarization'])
+    ms.selectchannel(start=inputs['channel'])
+    ms.select({'scan_number': inputs['scan_number']})
+    all_data = ms.getdata(inputs['selection'])
+    antenna1s = list(map(lambda antenna: int(antenna), all_data['antenna1']))
+    antenna2s = list(map(lambda antenna: int(antenna), all_data['antenna2']))
+    times = list(map(lambda time: float(time), all_data['time']))
+    amp_data = list(map(lambda amp: float(amp), all_data['amplitude'][0][0]))
+    return list(zip(antenna1s, antenna2s, times, amp_data))
+
+
+
 def main(args=None):
-    ms = casac.casac.ms ()
-    ms.open(data['input_location'])
-    with open(data['csv_location'], 'wb') as csvfile:
+    with open(inputs['csv_location'], 'wb') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-        for antenna1 in data['antenna1']:
-            for antenna2 in data['antenna2']:
-                ms.selectinit(reset=True)
-                ms.selectpolarization(data['polarization'])
-                ms.selectchannel(start=data['channel'])
-
-                if antenna1 == antenna2:
-                    continue
-                elif antenna1 < antenna2:
-                    primary_antenna = antenna1
-                    secondary_antenna = antenna2
-                else:
-                    primary_antenna = antenna2
-                    secondary_antenna = antenna1
-
-                ms.select({'scan_number': data['scan_number'], 'antenna1': primary_antenna, 'antenna2': secondary_antenna})
-                all_data = ms.getdata(data['selection'])
-                print all_data
-                # amp_data = all_data[data['selection']][0][0]
-                # time_data = all_data[data['selection'][1]][0][0]
-                # print "time=",time_data
-                # print amp_data
-                flattened_data = list(itertools.chain(*[[primary_antenna, secondary_antenna], amp_data]))
-                writer.writerow(flattened_data)
+        writer.writerows(get_data())
 
 
 main()
