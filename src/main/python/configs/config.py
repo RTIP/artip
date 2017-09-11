67d6353d501dfe32c5031e7f1e5b5cf7fcd799a9
Dollyg <dollyg@thoughtworks.com>
2017-09-11 17:05:00 +0530

    [#config_refactoring] Segregated stage toggles and sub-stage toggles.

--- src/main/python/configs/config.py
+++ src/main/python/configs/config.py
@@ -1,14 +1,16 @@
 from config_loader import ConfigLoader
 
 ALL_CONFIGS = None
+TARGET_SOURCE_CONFIGS = None
 GLOBAL_CONFIG = None
 CONFIG_PATH = None
 OUTPUT_PATH = None
 CASA_CONFIG = None
 
 def load(config_path):
-    global ALL_CONFIGS, GLOBAL_CONFIG, CONFIG_PATH, OUTPUT_PATH, CASA_CONFIG
+    global ALL_CONFIGS,TARGET_SOURCE_CONFIGS, GLOBAL_CONFIG, CONFIG_PATH, OUTPUT_PATH, CASA_CONFIG
     ALL_CONFIGS = ConfigLoader().load(config_path + "config.yml")
+    TARGET_SOURCE_CONFIGS = ConfigLoader().load(config_path + "target_source.yml")
     GLOBAL_CONFIG = ALL_CONFIGS['global']
     CONFIG_PATH = config_path
     OUTPUT_PATH = GLOBAL_CONFIG['output_path']
--- src/main/python/pipeline_stage.py
+++ src/main/python/pipeline_stage.py
@@ -14,7 +14,8 @@ from configs import pipeline_config
 
 class PipelineStage(object):
     STAGE_TOGGLES = pipeline_config.STAGES_TOGGLE_CONFIG
-    TARGET_SOURCE_TOGGLES = STAGE_TOGGLES['target_source']
+    TARGET_SOURCE_STAGES = config.TARGET_SOURCE_CONFIGS['target_source_stages']
+    CALIBRATION_STAGES = config.ALL_CONFIGS['calibration_stages']
 
     def __init__(self, measurement_set):
         self._measurement_set = measurement_set
@@ -30,7 +31,6 @@ class PipelineStage(object):
                         abs((end_time - start_time).seconds)) + " seconds" + Color.ENDC)
 
             return stage_func_wrapper
-
         return toggle_decorator
 
     @_run(STAGE_TOGGLES['flag_known_bad_data'])
@@ -45,55 +45,62 @@ class PipelineStage(object):
         logger.info(Color.SOURCE_HEADING + "Flux Calibration" + Color.ENDC)
         flux_calibrator = FluxCalibrator(self._measurement_set)
         flux_calibrator.run_setjy()
-        flux_calibrator.reduce_data()
+        if PipelineStage.CALIBRATION_STAGES['flux_calibration']['flagging']:
+            flux_calibrator.reduce_data()
+        else:
+            flux_calibrator.calibrate()
 
-    @_run(STAGE_TOGGLES['bandpass_calibration']['run_bandpass'])
+    @_run(STAGE_TOGGLES['bandpass_calibration'])
     def bandpass_calibration(self):
         logger.info(Color.SOURCE_HEADING + "Bandpass Calibration" + Color.ENDC)
         bandpass_calibrator = BandpassCalibrator(self._measurement_set)
         bandpass_calibrator.calibrate()
-        if pipeline_config.STAGES_TOGGLE_CONFIG['bandpass_calibration']['run_auto_flagging']:
+        if PipelineStage.CALIBRATION_STAGES['bandpass_calibration']['run_auto_flagging']:
             bandpass_calibrator.run_tfcrop()
             bandpass_calibrator.run_rflag()
-            bandpass_calibrator.calibrate()
+        bandpass_calibrator.calibrate()
 
     @_run(STAGE_TOGGLES['phase_calibration'])
     def phase_calibration(self):
         logger.info(Color.SOURCE_HEADING + "Phase Calibration" + Color.ENDC)
         phase_calibrator = PhaseCalibrator(self._measurement_set)
         phase_calibrator.calibrate()
-        phase_calibrator.reduce_data()
+        if PipelineStage.CALIBRATION_STAGES['phase_calibration']['flagging']:
+            phase_calibrator.reduce_data()
+        else:
+            phase_calibrator.calibrate()
 
+    @_run(STAGE_TOGGLES['target_source'])
     def target_source(self):
-        if self._target_source_toggle():
-            for source_id in config.GLOBAL_CONFIG['target_src_field']:
-                logger.info(Color.SOURCE_HEADING + "Target Source Calibration" + Color.ENDC)
-                target_source = TargetSource(self._measurement_set, source_id)
-                if self.TARGET_SOURCE_TOGGLES['calibrate']:
-                    target_source.calibrate()
-                line_source = LineSource(target_source.line(), source_id)
+        for source_id in config.GLOBAL_CONFIG['target_src_field']:
+            logger.info(Color.SOURCE_HEADING + "Target Source Calibration" + Color.ENDC)
+            target_source = TargetSource(self._measurement_set, source_id)
+            if self.TARGET_SOURCE_STAGES['calibrate']:
+                target_source.calibrate()
+            line_source = LineSource(target_source.line(), source_id)
+            self._create_ref_continuum_image(line_source, source_id)
+            self._run_autoflagging_on_line(line_source)
+            self._create_all_spw_continuum_image(line_source, source_id)
+            self._create_line_image(line_source)
 
-                self._create_ref_continuum_image(line_source, source_id)
-                self._run_autoflagging_on_line(line_source)
-                self._create_all_spw_continuum_image(line_source, source_id)
-                self._create_line_image(line_source)
-
-    @_run(TARGET_SOURCE_TOGGLES['reference_spw']['create_continuum'])
+    @_run(TARGET_SOURCE_STAGES['reference_spw']['create_continuum'])
     def _create_ref_continuum_image(self, line_source, source_id):
         cont_mode = 'ref'
         continuum_source_ref = ContinuumSource(
             line_source.continuum(config.GLOBAL_CONFIG['default_spw'], cont_mode),
             source_id, cont_mode)
         continuum_source_ref.reduce_data()
-        continuum_source_ref.self_calibrate(cont_mode)
         line_source.extend_continuum_flags()
+        if PipelineStage.TARGET_SOURCE_STAGES['reference_spw']['self_calibration']:
+            continuum_source_ref.self_calibrate(cont_mode)
 
-    @_run(TARGET_SOURCE_TOGGLES['all_spw']['run_auto_flagging'])
+
+    @_run(TARGET_SOURCE_STAGES['all_spw']['run_auto_flagging'])
     def _run_autoflagging_on_line(self, line_source):
         line_source.run_tfcrop()
         line_source.run_rflag()
 
-    @_run(TARGET_SOURCE_TOGGLES['all_spw']['create_continuum'])
+    @_run(TARGET_SOURCE_STAGES['all_spw']['create_continuum'])
     def _create_all_spw_continuum_image(self, line_source, source_id):
         if not self._is_single_spw_present():
             cont_mode = 'spw'
@@ -101,12 +108,13 @@ class PipelineStage(object):
             continuum_source = ContinuumSource(
                 line_source.continuum(spw_range, cont_mode),
                 source_id, cont_mode, spw_range)
-            continuum_source.self_calibrate(cont_mode)
+            if PipelineStage.TARGET_SOURCE_STAGES['all_spw']['self_calibration']:
+                continuum_source.self_calibrate(cont_mode)
         else:
             logger.info(
                 Color.HEADER + 'Spw continuum image is already created [spw range contains only one spw]' + Color.ENDC)
 
-    @_run(TARGET_SOURCE_TOGGLES['all_spw']['create_line_image'])
+    @_run(TARGET_SOURCE_STAGES['all_spw']['create_line_image'])
     def _create_line_image(self, line_source):
         cont_mode = 'ref' if self._is_single_spw_present() else 'spw'
         line_source.apply_calibration(cont_mode)
@@ -114,8 +122,3 @@ class PipelineStage(object):
 
     def _is_single_spw_present(self):
         return config.GLOBAL_CONFIG['spw_range'].find(",") == -1
-
-    def _target_source_toggle(self):
-        return self.TARGET_SOURCE_TOGGLES['all_spw']['create_line_image'] or self.TARGET_SOURCE_TOGGLES['all_spw'][
-            'create_continuum'] or self.TARGET_SOURCE_TOGGLES['all_spw']['run_auto_flagging'] or \
-               self.TARGET_SOURCE_TOGGLES['reference_spw']['create_continuum']
