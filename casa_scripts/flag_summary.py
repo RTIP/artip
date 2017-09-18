52b149a185e8590485e66d336c70cf3ed860bb99
Sarang Sanjay Kulkarni <ksarang@thoughtworks.com>
2017-09-18 12:48:19 +0530

    Refactoring for performance optimization. Using tinydb for current version.

--- casa_scripts/flag_summary.py
+++ casa_scripts/flag_summary.py
@@ -1,53 +1,26 @@
 import sys
-import json
+from tinydb import TinyDB, Query
 
-polarizations = sys.argv[-6]
+polarizations = sys.argv[-6].split(",")
 ms_dataset = sys.argv[-5]
 output_path = sys.argv[-4]
 flagging_type = sys.argv[-3]
 scans = sys.argv[-2].split(",")
 source_type = sys.argv[-1]
 
-json_file = '{0}/flag_summary.json'.format(output_path)
+db_file = '{0}/flag_summary.json'.format(output_path)
 dataset_name = ms_dataset.split("/")[-1]
 
-datasets = {}
+db = TinyDB(db_file)
+dataset = Query()
 
+rows = []
+for scan in scans:
+    for pol in polarizations:
+        summary = flagdata(vis=ms_dataset, mode='summary', scan=scan, correlation=pol)
+        antenna = summary["antenna"]
+        rows.append(
+            {'dataset': dataset_name, 'source_type': source_type, "scan": scan, "pol": pol, flagging_type: antenna})
 
-def create_or_load_file():
-    global datasets
-    try:
-        with open(json_file) as data_file:
-            datasets = json.load(data_file)
-    except IOError:
-        open(json_file, "w+")
-
-
-create_or_load_file()
-
-
-def create_key_or_get_value(data, key):
-    if key not in data:
-        data[key] = {}
-    return data[key]
-
-
-data = {}
-data[dataset_name] = {}
-if dataset_name in datasets.keys():
-    data[dataset_name] = datasets[dataset_name]
-dataset = data[dataset_name]
-for scan_no in scans:
-    scan_data = create_key_or_get_value(dataset, scan_no)
-    scan_data["source_type"] = source_type
-    polarization_data = create_key_or_get_value(scan_data, "polarization")
-    for pol in polarizations.split(","):
-        summary = flagdata(vis=ms_dataset, mode='summary', scan=scan_no, correlation=pol)
-        pol_data = create_key_or_get_value(polarization_data, pol)
-        if flagging_type not in pol_data:
-            pol_data[flagging_type] = summary
-
-datasets.update(data)
-
-with open(json_file, 'w') as outfile:
-    json.dump(datasets, outfile)
+db.insert_multiple(rows)
+db.close()
--- flagged_data_summary/flagging_summary.js
+++ flagged_data_summary/flagging_summary.js
@@ -1,28 +1,28 @@
 var input = {}
 
-function antenna_list(data){
-    antennas= []
+function antenna_list(data) {
+    antennas = []
     data.forEach(function(row) {
         antennas.push(row["antenna"]);
     });
     return antennas
 }
 
-function generate_label(source_type,scan, pol){
+function generate_label(source_type, scan, pol) {
     return `Scan : ${scan}, ${pol}`
 }
 
-function generate_graph(pol_data, source_type, bind, scan, pol){
-    var antennas = pol_data[pol]["antenna"]
+function generate_graph(pol_data, source_type, bind, scan, pol) {
+    var antennas = pol_data[pol]
     var chart = c3.generate({
         bindto: bind,
         data: {
             json: antennas,
             keys: {
-                    value: ['known_flags', 'rang_closure', 'detailed_flagging', 'rflag', 'tfcrop']
-                },
+                value: ['known_flags', 'rang_closure', 'detailed_flagging', 'rflag', 'tfcrop']
+            },
             names: {
-                 detailed_flagging: 'Detail Flagging',
+                detailed_flagging: 'Detail Flagging',
                 rang_closure: 'Closure & Rang',
                 known_flags: 'Known Flags',
                 antenna: 'Antenna',
@@ -38,21 +38,28 @@ function generate_graph(pol_data, source_type, bind, scan, pol){
             order: null
         },
         title: {
-            text: generate_label(source_type,scan, pol),
+            text: generate_label(source_type, scan, pol),
             y: 100,
-            padding: {top:10, bottom:16, left: 70},
+            padding: {
+                top: 10,
+                bottom: 16,
+                left: 70
+            },
             position: 'bottom'
         },
 
         axis: {
             y: {
                 label: {
-                    text:'Percentage -->',
+                    text: 'Percentage -->',
                     position: 'outer-middle'
                 },
-                max:100,
-                min:0,
-                padding: {top: 0, bottom: 0},
+                max: 100,
+                min: 0,
+                padding: {
+                    top: 0,
+                    bottom: 0
+                },
                 tick: {
                     count: 5
                 },
@@ -68,45 +75,53 @@ function generate_graph(pol_data, source_type, bind, scan, pol){
             }
         },
         grid: {
-          y: {
-            show: true
-          }
+            y: {
+                show: true
+            }
         },
-         tooltip: {
-        format: {
-            value: function(value) {
-                return d3.format(",.2f")(value)+'%'
+        tooltip: {
+            format: {
+                value: function(value) {
+                    return d3.format(",.2f")(value) + '%'
+                }
             }
         }
-    }
 
     });
 }
 
-function loadJson(sel_source_type) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      datasets = JSON.parse(this.responseText);
-      for(var graph_data of datasets){
-          for (var scan in graph_data) {
-            source_type = graph_data[scan]["source_type"]
-            if(sel_source_type.includes(source_type)){
-                var polarizations = graph_data[scan]["polarization"]
-                for(var pol in polarizations){
-                   var aDiv = document.createElement('div');
-                   aDiv.id = "chart_"+pol+ scan;
-                   document.getElementById("container").appendChild(aDiv);
-                   generate_graph(polarizations, source_type, '#'+aDiv.id, scan, pol)
-                }
-            }
-          }
-      }
+function displayChart(sel_source_type, source_type) {
+    return sel_source_type.every(function(val) {
+        return source_type.indexOf(val) >= 0;
+    });
+}
 
-    }
-  };
-  xhttp.open("GET", "./graph.json", true);
-  xhttp.send();
+function loadJson(sel_source_type) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            datasets = JSON.parse(this.responseText);
+            Object.entries(datasets).forEach(
+                ([dataset_name, graph_data]) => {
+                    for (var scan in graph_data) {
+                        source_type = graph_data[scan]["source_type"]
+                        if (displayChart(sel_source_type, source_type)) {
+                            var polarizations = graph_data[scan]["polarization"]
+                            for (var pol in polarizations) {
+                                var aDiv = document.createElement('div');
+                                aDiv.id = "chart_" + pol + scan;
+                                document.getElementById("container").appendChild(aDiv);
+                                generate_graph(polarizations, source_type, '#' + aDiv.id, scan, pol)
+                            }
+                        }
+                    }
+
+                }
+            );
+        }
+    };
+    xhttp.open("GET", "./graph.json", true);
+    xhttp.send();
 }
 
 loadJson(["flux_calibrator", "bandpass_calibrator"]);
